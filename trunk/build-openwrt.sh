0d6ba82944490f598b5eb92078addf9419f3105f
rssnsj@gmail.com <rssnsj@gmail.com@5a70e73d-ab1a-b34c-e4f6-f6dd0366c5d7>
2013-08-20 15:35:00 +0000

    build-openwrt.sh: Add more operations to this script, still unfinished.

--- trunk/build-openwrt.sh
+++ trunk/build-openwrt.sh
@@ -1,8 +1,12 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 cd `dirname $0`
 
 hiwifi_root=`pwd`
+
+root_squashfs=openwrt-ar71xx-generic-root.squashfs
+official_fw=recovery.bin
+target_fw=openwrt-tw150v1-recovery.bin
 
 # 1. Install required components:
 for deb in build-essential flex gettext libncurses5-dev unzip gawk liblzma-dev; do
@@ -12,30 +16,39 @@ done
 # 2. Checkout source code:
 [ -e openwrt-ar9331 ] || svn co svn://svn.openwrt.org/openwrt/trunk openwrt-ar9331 -r36145
 
-cd openwrt-ar9331
+# Operations under the OpenWrt source directory
+(
+	cd openwrt-ar9331
 
-# 3. Add LuCI (web GUI) for compiling:
-./scripts/feeds update
-./scripts/feeds install -a -p luci
+	# 3. Add LuCI (web GUI) for compiling:
+	./scripts/feeds update
+	./scripts/feeds install -a -p luci
 
-# 4. Add 'libevent2' (not necessary):
-./scripts/feeds install libevent2
+	# 4. Add 'libevent2' (not necessary):
+	./scripts/feeds install libevent2
 
-# 5. Add 'tw150v1' platform support: <<<< IMPORTANT
-mach_to_replace=target/linux/ar71xx/files/arch/mips/ath79/mach-ap83.c
-if ! [ -L $mach_to_replace -a -e $mach_to_replace ]; then
-	rm -vf $mach_to_replace
-	ln -sv "$hiwifi_root/mach-tw150v1/mach-ap83.c" $mach_to_replace
-fi
+	# 5. Add 'tw150v1' platform support: <<<< IMPORTANT
+	mach_to_replace=target/linux/ar71xx/files/arch/mips/ath79/mach-ap83.c
+	if ! [ -L $mach_to_replace -a -e $mach_to_replace ]; then
+		rm -vf $mach_to_replace
+		ln -sv "$hiwifi_root/mach-tw150v1/mach-ap83.c" $mach_to_replace
+	fi
 
-# 6. Import the source code configuration
-[ -e .config ] || cp -vf "$hiwifi_root"/config-ar9xxx-mach-ap83 .config
+	# 6. Import the source code configuration
+	[ -e .config ] || cp -vf "$hiwifi_root"/config-ar9xxx-mach-ap83 .config
 
-exit 0
-
-# 7. Build imagess for AP83 platform that we selected:
-make V=99
+	if ! [ -f openwrt-ar9331/build_dir/target-mips*/linux-ar71xx_generic/vmlinux -a \
+		-f openwrt-ar9331/bin/ar71xx/$root_squashfs ]; then
+		# 7. Build imagess for AP83 platform that we selected:
+		make V=99
+	fi
+)
 
 # 8. Generate firmware image for HiWiFi board:
-cd "$hiwifi_root"
+(
+	export openwrt_root="$hiwifi_root/openwrt-ar9331"
+	cd firmware-builder
+	./tw150v1-buildfw.sh
+	mv -vf openwrt-tw150v1-recovery.bin ../
+) || exit $?
 
---
+++ trunk/firmware-builder/tw150v1-buildfw.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+[ -z "$openwrt_root" ] || openwrt_root=~/openwrt-ar9331
+
+root_squashfs=openwrt-ar71xx-generic-root.squashfs
+official_fw=recovery.bin
+target_fw=openwrt-tw150v1-recovery.bin
+
+# 'prefix-1-2.bin' is required, get it from recovery.bin
+if [ ! -f prefix-1-2.bin ]; then
+	# Get first 0x20000 bytes: u-boot, bdinfo
+	( set -x; head "$official_fw" -c131072 > prefix-1-2.bin )
+fi
+
+set -x
+
+cp -vf $openwrt_root/build_dir/target-mips*/linux-ar71xx_generic/vmlinux   vmlinux-hiwifi-tw150v1
+$openwrt_root/host/bin/patch-cmdline vmlinux-hiwifi-tw150v1   "board=tw150v1 console=ttyATH0,115200"
+$openwrt_root/staging_dir/host/bin/lzma   e vmlinux-hiwifi-tw150v1 -lc1 -lp2 -pb2 vmlinux-hiwifi-tw150v1.bin.lzma
+
+cp $openwrt_root/bin/ar71xx/$root_squashfs ./
+
+mkimage -A mips -O linux -T kernel -a 0x80060000 -C lzma  -e 0x80060000 -n 'tw150v1-Linux-3.8.3' -d vmlinux-hiwifi-tw150v1.bin.lzma uImage
+
+raw_size=`stat --format="%s" uImage`
+pad_size=`expr 1310720 - $raw_size`
+dd if=/dev/zero of=__pad.tmp bs=$pad_size count=1
+cat uImage __pad.tmp > uImage.1280k
+rm -f __pad.tmp
+
+cat prefix-1-2.bin uImage.1280k $root_squashfs > $target_fw
+
+cp -vf $target_fw /tftpboot/recovery.bin
+
